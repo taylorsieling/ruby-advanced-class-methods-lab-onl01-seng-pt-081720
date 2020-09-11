403145a4013f616b75d4009bbece78ddf7fca4f8
Taylor Sieling <taylorasieling@gmail.com>
2020-09-11 17:22:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
   end 
   
   def self.create_from_filename(filename)
-     new_file = filename.split(" - ")
+    new_file = filename.split(" - ")
     song_artist = new_file[0]
     song_name = new_file[1].gsub(".mp3","")
     song = self.create
