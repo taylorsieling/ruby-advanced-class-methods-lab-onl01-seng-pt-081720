2e715d4c12b74e7086e8c0405193bc31597e0e33
Taylor Sieling <taylorasieling@gmail.com>
2020-09-11 17:04:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,50 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find { |song| song.name == name }
+  end 
+  
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end 
+  
+  def self.alphabetical
+    sorted = self.all.sort_by { |song| song.name }
+    sorted
+  end 
+  
+  def self.new_from_filename(filename)
+    new_file = filename.split("-")
+    song.artist = [0]
+    song.name = [1]
+  end 
+  
+  def self.create_from_filename(filename)
+    new_file = filename.split("-")
+  end 
+  
+  def self.destroy_all
+    self.all.clear
+  end
 
 end
